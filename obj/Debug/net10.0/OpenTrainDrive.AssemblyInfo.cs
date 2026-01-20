dcd09c713778562bc9bfbd605f819d18fb9d57e1
Hansueli Alder <hansueli.alder@batec.net>
2026-01-20 12:13:01 +0100

    train.xml ebenfalls bereinigt, inkl. Doku

--- obj/Debug/net10.0/OpenTrainDrive.AssemblyInfo.cs
+++ obj/Debug/net10.0/OpenTrainDrive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d60e2ac92b838bd2a60fb2d3d6c09d48cb322cbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7713770f9f4d6f7271927a8f3fe43c6e673ba777")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
