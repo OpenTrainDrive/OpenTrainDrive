4cbda980e211103d33aef875c5f6cdd80e8ca0ca
Siald20 <103524712+Siald20@users.noreply.github.com>
2026-01-21 18:46:18 +0100

    Ordner Engine verschoben

--- obj/Debug/net10.0/OpenTrainDrive.AssemblyInfo.cs
+++ obj/Debug/net10.0/OpenTrainDrive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0cca490f1ad97d9030fd56f5f404c371f786af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ff38027c8d5ea4da1c3829797fe56512ab1518")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
